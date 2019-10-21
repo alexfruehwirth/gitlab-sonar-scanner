1d23744c1a25969ab780d5507b351c94b684be98
Alexander Frühwirth <alexander.fruehwirth@gmail.com>
2019-10-21 15:16:09 +0200

    Change runner script to allow sonarqube7.7

--- sonar-scanner-run.sh
+++ sonar-scanner-run.sh
@@ -55,12 +55,7 @@ if [ ! -z ${SONAR_BRANCH+x} ]; then
   COMMAND="$COMMAND -Dsonar.branch.name=$SONAR_BRANCH"
 fi
 
-# analysis by default
-if [ -z ${SONAR_ANALYSIS_MODE+x} ]; then
-  SONAR_ANALYSIS_MODE="preview"
-fi
 
-COMMAND="$COMMAND -Dsonar.analysis.mode=$SONAR_ANALYSIS_MODE"
 if [ $SONAR_ANALYSIS_MODE == "preview" ]; then
   COMMAND="$COMMAND -Dsonar.issuesReport.console.enable=true"
   COMMAND="$COMMAND -Dsonar.gitlab.failure_notification_mode=exit-code"
